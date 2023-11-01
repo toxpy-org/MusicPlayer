09fa97086f2b4f6358675fa204b89a54d23ed616
natoxpy <natoxpy@gmail.com>
2023-11-01 00:07:44 -0400

    Deployment configured

--- next.config.js
+++ next.config.js
@@ -1,11 +1,29 @@
-const path = require('path')
+const path = require("path");
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-}
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: 'the "domain" of your Imigix source',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
